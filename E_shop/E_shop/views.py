4acadd4b5d1ef669a80ecfe717b29d6632b9ad48
Lazizjon <lazizjannsl12@gmail.com>
2024-08-01 17:52:51 +0500

    FINISH

---
+++ E_shop/E_shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from store_app.models import Product
+def BASE(request):
+    return  render(request, 'Main/base.html')    
+def HOME(request):
+    product=Product.objects.filter(status='Publish')  
+    context={
+        'product':product
+    }
+    return  render(request, 'Main/index.html',context)
+
+def PRODUCT(request):
+    product=Product.objects.filter(status='Publish')  
+    context={
+        'product':product
+    }
+    return render(request, 'Main/product.html',context)
