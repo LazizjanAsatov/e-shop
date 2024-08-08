53acff7395404e247f49e3e49067547674153dc9
Lazizjon <lazizjannsl12@gmail.com>
2024-08-08 16:35:14 +0500

    2 commit

---
+++ store_app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from . models import *
+
+class ImagesTublerinlineAdmin(admin.TabularInline):
+    model = Images
+
+class TagTublerinlineAdmin(admin.TabularInline):
+    model = Tag
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ImagesTublerinlineAdmin,TagTublerinlineAdmin]
+
+
+admin.site.register(Images)
+admin.site.register(Tag)
+admin.site.register(Categories)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Filter_Price)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Contact_Us)
